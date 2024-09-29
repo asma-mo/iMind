361b6c10eb68a2c478f127dea9784c56168c8edd
asma <Asma.mo@outlook.com>
2024-09-29 14:10:08 +0300

    Initial Commit

---
+++ challenge1/challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  challenge1App.swift
+//  challenge1
+//
+//  Created by Asma on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ challenge1Tests/challenge1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  challenge1Tests.swift
+//  challenge1Tests
+//
+//  Created by Asma on 29/09/2024.
+//
+
+import Testing
+@testable import challenge1
+
+struct challenge1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
